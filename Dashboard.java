2eb34ed401f4021eb0bd9a0b5694820f0d824f28
iBiL-007 <grtk.annan7@gmail.com>
2017-03-19 11:20:11 -0500

    Add files via upload

---
+++ Dashboard.java
@@ -0,0 +1,13 @@
+package views;
+//DASHBOARD CODE GOES HERE
+public class Dashboard {
+	
+	
+	public static void getStudentData(models.Student sobj){
+		//updateDashboard(sobj);
+	}
+	public void updateDashboard(models.Student sobj){
+		//.....
+	}
+
+}
